09e0ee7fe50b1cb471c0537b70884f64919ba956
Martin Silva <martinsilva@flowics.com>
2016-10-04 10:36:30 -0300

    example app

---
+++ example-app/store.js
@@ -0,0 +1,3 @@
+import { createStore } from 'redux';
+import todos from './modules/todos'
+export default createStore(todos)
---
+++ example-app/webpack.config.js
@@ -0,0 +1,24 @@
+var path = require("path");
+module.exports = {
+  entry: './components/app.js',
+  output: {
+    path: path.resolve(__dirname, "build"),
+    publicPath: "/",
+    filename: "bundle.js"
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel',
+        query: {
+          presets: ['react', 'es2015']
+        }
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['', '.js']
+  }
+};
