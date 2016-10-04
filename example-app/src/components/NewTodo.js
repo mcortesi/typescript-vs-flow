0caa51f4eef41b9e89582a1f5ba13b451bbfc531
Martin Silva <martinsilva@flowics.com>
2016-10-04 14:50:43 -0300

    remove unused

--- example-app/src/components/NewTodo.js
+++ example-app/src/components/NewTodo.js
@@ -2,11 +2,13 @@
 import React from 'react'
 import { connect } from 'react-redux'
 
-const NewTodo = ({ onChange } : { onChange: ( event: any ) => void }) => (
+function NewTodo({ onChange } : { onChange: ( event: any ) => void }) {
+  return (
   <div>
     <h3>New</h3>
     <input type="text" onKeyUp={onChange}/>
   </div>
-)
+  )
+}
 
 export default NewTodo
