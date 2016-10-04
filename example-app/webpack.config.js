022e67bbdca6bc61c5d2469f001605ce9fc22516
Mariano A. Cortesi <mariano@flowics.com>
2016-10-04 15:02:05 -0300

    Add typsecript support to webpack

--- example-app/webpack.config.js
+++ example-app/webpack.config.js
@@ -15,10 +15,16 @@ module.exports = {
         query: {
           presets: ['react', 'es2015']
         }
-      }
+      },
+      { test: /\.tsx?$/, loader: "ts-loader" }
+    ],
+    preLoaders: [
+      // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
+      { test: /\.js$/, loader: "source-map-loader" }
     ]
   },
   resolve: {
-    extensions: ['', '.js']
-  }
+    extensions: ["", ".webpack.js", ".web.js", ".ts", ".tsx", ".js"]
+  },
+  devtool: "source-map",
 };
